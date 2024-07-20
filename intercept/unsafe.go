5006c664eccd44ff49a9e832132e64367ae6865d
Restart <45609733+RestartFU@users.noreply.github.com>
2024-07-20 13:11:11 -0400

    Update unsafe.go

--- intercept/unsafe.go
+++ intercept/unsafe.go
@@ -1,30 +1,29 @@
 package intercept
 
 import (
-    "github.com/df-mc/dragonfly/server/player"
-    "github.com/df-mc/dragonfly/server/session"
-    "github.com/sandertv/gophertunnel/minecraft/protocol/packet"
-    "reflect"
-    "unsafe"
+	"github.com/df-mc/dragonfly/server/player"
+	"github.com/df-mc/dragonfly/server/session"
+	"reflect"
+	"unsafe"
 )
 
 // updatePrivateField sets a private field of a session to the value passed.
 func updatePrivateField[T any](s *session.Session, name string, value T) {
-    reflectedValue := reflect.ValueOf(s).Elem()
-    privateFieldValue := reflectedValue.FieldByName(name)
+	reflectedValue := reflect.ValueOf(s).Elem()
+	privateFieldValue := reflectedValue.FieldByName(name)
 
-    privateFieldValue = reflect.NewAt(privateFieldValue.Type(), unsafe.Pointer(privateFieldValue.UnsafeAddr())).Elem()
+	privateFieldValue = reflect.NewAt(privateFieldValue.Type(), unsafe.Pointer(privateFieldValue.UnsafeAddr())).Elem()
 
-    privateFieldValue.Set(reflect.ValueOf(value))
+	privateFieldValue.Set(reflect.ValueOf(value))
 }
 
 // fetchPrivateField fetches a private field of a session.
 func fetchPrivateField[T any](s *session.Session, name string) T {
-    reflectedValue := reflect.ValueOf(s).Elem()
-    privateFieldValue := reflectedValue.FieldByName(name)
-    privateFieldValue = reflect.NewAt(privateFieldValue.Type(), unsafe.Pointer(privateFieldValue.UnsafeAddr())).Elem()
+	reflectedValue := reflect.ValueOf(s).Elem()
+	privateFieldValue := reflectedValue.FieldByName(name)
+	privateFieldValue = reflect.NewAt(privateFieldValue.Type(), unsafe.Pointer(privateFieldValue.UnsafeAddr())).Elem()
 
-    return privateFieldValue.Interface().(T)
+	return privateFieldValue.Interface().(T)
 }
 
 // noinspection ALL
